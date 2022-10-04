a40ad8d516a1ca1f4da2a662bdb58d277a9932c4
Raflhy Nur Ramadhan <raflhy.pragma@gmail.com>
2022-10-04 10:39:10 +0700

    Menambah menu kondisi aset dan team

--- routes/web.php
+++ routes/web.php
@@ -158,6 +158,14 @@ Route::middleware('auth')
                                     Route::grid('district', 'DistrictController');
                                 }
                             );
+
+                            Route::get('kondisi-aset/import', 'KondisiAsetController@import')->name('kondisi-aset.import');
+                            Route::post('kondisi-aset/importSave', 'KondisiAsetController@importSave')->name('kondisi-aset.importSave');
+                            Route::grid('kondisi-aset', 'KondisiAsetController');
+
+                            Route::get('team/import', 'TeamController@import')->name('team.import');
+                            Route::post('team/importSave', 'TeamController@importSave')->name('team.importSave');
+                            Route::grid('team', 'TeamController');
                     }
                 );
 
