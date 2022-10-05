b958f4c08d562f00ddfb5babbd40e1ee4945c965
Rusman Wahab <hwalyonglance@gmail.com>
2022-10-05 10:17:14 +0700

    fix

--- resources/views/master/status-aset/create.blade.php
+++ resources/views/master/status-aset/create.blade.php
@@ -5,9 +5,9 @@
 @section('modal-body')
 	@method('POST')
 	<div class="form-group row">
-		<label class="col-sm-12 col-form-label">{{ __('Nama') }}</label>
+		<label class="col-sm-12 col-form-label">{{ __('Status Aset') }}</label>
 		<div class="col-sm-12 parent-group">
-			<input type="text" name="name" class="form-control" placeholder="{{ __('Nama') }}">
+			<input type="text" name="name" class="form-control" placeholder="{{ __('Status Aset') }}">
 		</div>
 	</div>
 @endsection
